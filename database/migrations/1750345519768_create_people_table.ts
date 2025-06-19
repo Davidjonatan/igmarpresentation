38dc73e087e5b18f80aa4898660b1b487be46f7c
jonadick <158613571+Davidjonatan@users.noreply.github.com>
2025-06-19 12:12:31 -0600

    se arreglo lo de migrations

--- database/migrations/1750345519768_create_people_table.ts
+++ database/migrations/1750345519768_create_people_table.ts
@@ -1,22 +1,20 @@
-// database/migrations/..._persons.ts
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'persons'
+  protected tableName = 'people'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      
       table.string('name', 80).notNullable()
       table.string('lastname', 80).notNullable()
       table.integer('age').unsigned()
       table.string('email', 254).notNullable().unique()
       table.string('phone', 20).nullable()
       
-      // Solución para timestamp en MySQL
-      table.timestamp('created_at').notNullable().defaultTo(this.now())
-      table.timestamp('updated_at').notNullable().defaultTo(this.now())
+      // SOLUCIÓN: Usar datetime
+      table.dateTime('created_at').notNullable()
+      table.dateTime('updated_at').notNullable()
     })
   }
 
